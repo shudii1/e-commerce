89ea37cb5decd2f214e9c98572644dd7ff66580c
shudii1 <hustle162@gmail.com>
2020-11-21 11:26:36 +0530

    Intial commit

---
+++ mac/mac/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+#from .models import Product, Contact, Orders
+from math import ceil
+
+
+# Create your views here.
+def index(request):
+
+    return render(request, 'index.html')
+
